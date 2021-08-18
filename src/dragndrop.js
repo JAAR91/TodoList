7dbf33f26c9307e8ed05eaf9b5f5bb522fae79ca
JAAR91 <jaarriagaramos@gmail.com>
2021-08-17 18:13:53 -0600

    fix the linters and bus that prevent the item to be drag and drop

--- src/dragndrop.js
+++ src/dragndrop.js
@@ -1,67 +1,58 @@
 const dragNdrop = (container, myTodoList) => {
-    var dragedItem = null;
-    const addAttributes = (item, size) => {
-        
-
-        item.addEventListener('dragstart', (e) => {
-            item.opacity = '0.4';
-            dragedItem = item;
-            e.dataTransfer.effectAllowed = 'move';
-            e.dataTransfer.setData('text/html', item.innerHTML);
-        });
-
-        item.addEventListener('dragover', (e) => {
-            if (e.preventDefault) {
-                e.preventDefault();
-            }
-            e.dataTransfer.dropEffect = 'move';
-            return false;
-            
-        });
-
-        item.addEventListener('dragenter', (e) => {
-            if (dragedItem !== item){
-                item.classList.remove('p-3');
-                item.style.paddingTop = '80px';
-                item.style.backgroundImage = `linear-gradient(rgb(76, 89, 151), white, white)`;
-            }
-        });
-
-        item.addEventListener('dragleave', (e) => {
-            item.classList.add('p-3');
-            item.style.backgroundImage = `linear-gradient(white, white)`;
-        });
-
-        item.addEventListener('drop', (e) => {
-            if (e.stopPropagation) {
-                e.stopPropagation(); 
-            }
-            if (dragedItem != item) {
-                dragedItem.innerHTML = item.innerHTML;
-                item.innerHTML = e.dataTransfer.getData('text/html');
-                myTodoList.swap(item.querySelector('input').value - 1,
-                dragedItem.querySelector('input').value - 1);
-            }
-                
-            return false;
-        });
-
-        item.addEventListener('dragend', (e) => {
-            item.style.opacity = '1';
-    
-            items.forEach(function (item) {
-                item.classList.add('p-3');
-                item.style.backgroundImage = `linear-gradient(white, white)`;
-            });
-        });
-    };
-
-    let items = container.querySelectorAll('li');
-    let size = items.length;
-    items.forEach((item, index) => {
-        addAttributes(item, size, index);
+  const items = container.querySelectorAll('li');
+  let replaceItem = null;
+  items.forEach((item) => {
+    item.addEventListener('dragstart', (e) => {
+      item.opacity = '0.4';
+      replaceItem = item;
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', item.innerHTML);
     });
-    
+
+    item.addEventListener('dragover', (e) => {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+      e.dataTransfer.dropEffect = 'move';
+      return false;
+    });
+
+    item.addEventListener('dragenter', () => {
+      if (replaceItem !== item) {
+        item.classList.remove('p-3');
+        item.style.paddingTop = '80px';
+        item.style.backgroundImage = 'linear-gradient(rgb(76, 89, 151), white, white)';
+      }
+    });
+
+    item.addEventListener('dragleave', () => {
+      item.classList.add('p-3');
+      item.style.backgroundImage = 'linear-gradient(white, white)';
+    });
+
+    item.addEventListener('drop', (e) => {
+      if (e.stopPropagation) {
+        e.stopPropagation();
+      }
+      if (replaceItem !== item) {
+        replaceItem.innerHTML = item.innerHTML;
+        item.innerHTML = e.dataTransfer.getData('text/html');
+        myTodoList.swap(item.querySelector('input').value,
+          replaceItem.querySelector('input').value);
+      }
+
+      return false;
+    });
+
+    item.addEventListener('dragend', () => {
+      item.style.opacity = '1';
+
+      items.forEach((item) => {
+        item.classList.add('p-3');
+        item.style.backgroundImage = 'linear-gradient(white, white)';
+      });
+    });
+  });
 };
 
 export default dragNdrop;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,62 @@
 import './styles/bootstrap.css';
 import './styles/style.css';
 import Container from './domLoader.js';
-import dragNdrop from './dragndrop.js'
-import myTodoList from './constructor.js'
+import dragNdrop from './dragndrop.js';
+import myTodoList from './constructor.js';
 
-myTodoList.new(false, `Click on the text to edit`);
-myTodoList.new(false, `Drag 'n drop to reorder your list`);
-myTodoList.new(false, `Manage all your lists in one place`);
-myTodoList.new(false, `Resync to clear out the old`);
+myTodoList.new(false, 'Click on the text to edit');
+myTodoList.new(false, 'Drag \'n drop to reorder your list');
+myTodoList.new(false, 'Manage all your lists in one place');
+myTodoList.new(false, 'Resync to clear out the old');
 
-const deleteTodo = (value) => {
-    myTodoList.delete(value);
-    printTodoList();
-};
+const printTodoList = () => {
+  Container.innerHTML = '';
+  const todosNform = document.createElement('div');
 
-const printTodo = (item, TodosContainer) => {
+  const formContainer = document.createElement('ul');
+  formContainer.classList.add('d-flex', 'flex-column',
+    'p-2', 'm-0', 'list-group');
+  todosNform.appendChild(formContainer);
+
+  const liContainer1 = document.createElement('li');
+  liContainer1.classList.add('list-group-item');
+  todosNform.appendChild(liContainer1);
+
+  const todoLabel = document.createElement('label');
+  todoLabel.textContent = 'Demo';
+  liContainer1.appendChild(todoLabel);
+
+  const liContainer2 = document.createElement('li');
+  liContainer2.classList.add('list-group-item');
+  todosNform.appendChild(liContainer2);
+
+  const todoInput = document.createElement('input');
+  todoInput.classList.add('todoInput');
+  todoInput.placeholder = 'Add to your list...';
+  todoInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      myTodoList.new(false, todoInput.value);
+      printTodoList();
+    }
+  });
+  liContainer2.appendChild(todoInput);
+
+  const TodosContainer = document.createElement('ul');
+  TodosContainer.classList.add('list-group', 'bg-dark', 'mt-2');
+
+  TodosContainer.innerHTML = '';
+
+  myTodoList.list.forEach((item) => {
     const Todo = document.createElement('li');
-    Todo.classList.add('list-group-item', 'd-flex', 'align-items-center'
-    ,'p-3', 'justify-content-between');
+    Todo.classList.add('list-group-item', 'd-flex', 'align-items-center',
+      'p-3', 'justify-content-between');
     Todo.draggable = true;
-    
+
+    const indexInput = document.createElement('input');
+    indexInput.type = 'hidden';
+    indexInput.value = item.index;
+    Todo.appendChild(indexInput);
+
     const checkInput = document.createElement('input');
     checkInput.classList.add('m-0');
     checkInput.type = 'checkbox';
@@ -35,65 +72,17 @@ const printTodo = (item, TodosContainer) => {
     deleteButton.classList.add('deletebutton');
     Todo.appendChild(deleteButton);
 
-    deleteButton.addEventListener('click', ()=> {
-        deleteTodo(item);
+    deleteButton.addEventListener('click', () => {
+      myTodoList.delete(indexInput.value);
+      printTodoList();
     });
 
     TodosContainer.appendChild(Todo);
-};
+  });
+  todosNform.appendChild(TodosContainer);
+  Container.appendChild(todosNform);
 
-const addTodo = (e, value) => {
-    if (e.key === 'Enter') {
-        myTodoList.new(false, value);
-        printTodoList();
-    }
-};
-
-const printTodoList = () => {
-    Container.innerHTML = ``;
-    const todosNform = document.createElement('div');
-
-    const formContainer = document.createElement('ul');
-    formContainer.classList.add('d-flex', 'flex-column'
-    , 'p-2', 'm-0','list-group');
-    todosNform.appendChild(formContainer);
-
-    const liContainer1 = document.createElement('li');
-    liContainer1.classList.add('list-group-item');
-    todosNform.appendChild(liContainer1);
-
-    const todoLabel = document.createElement('label');
-    todoLabel.textContent = 'Demo';
-    liContainer1.appendChild(todoLabel);
-
-    const liContainer2 = document.createElement('li');
-    liContainer2.classList.add('list-group-item');
-    todosNform.appendChild(liContainer2);
-
-    const todoInput = document.createElement('input');
-    todoInput.classList.add('todoInput');
-
-    todoInput.placeholder = 'Add to your list...';
-    liContainer2.appendChild(todoInput);
-
-    const TodosContainer = document.createElement('ul');
-    TodosContainer.classList.add('list-group', 'bg-dark', 'mt-2');
-
-    TodosContainer.innerHTML = ``;
-
-    myTodoList.list.forEach((item)=> {
-        printTodo(item, TodosContainer);
-    });
-    todosNform.appendChild(TodosContainer);
-    Container.appendChild(todosNform);
-
-
-    TodosContainer.ondragstart = dragNdrop(TodosContainer, myTodoList);
-    
-
-    todoInput.addEventListener('keypress', (e) => {
-        addTodo(e, todoInput.value);
-    });
+  TodosContainer.ondragstart = dragNdrop(TodosContainer, myTodoList);
 };
 
 printTodoList();
