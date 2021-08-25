9ed08fe32c068df09f164132021c0673d35b44c4
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-08-25 17:12:10 -0400

    Add tests for new, indexControl, delete, & swap

--- src/constructor.test.js
+++ src/constructor.test.js
@@ -11,4 +11,28 @@ describe('Testing constructor class functions', () => {
         new Todo(3, false, 'Manage all your lists in one place'),
         new Todo(4, false, 'Resync to clear out the old')]);
     });
+
+    test('Add new Todo objects to local Storage', () => {
+      myTodoList.list = [];
+      myTodoList.new('Task 1');
+      myTodoList.new('Task 2');
+      expect(myTodoList.list).toEqual([new Todo(1, false, 'Task 1'),
+      new Todo(2, false, 'Task 2')]);
+    });
+
+    test('Get highest index in Todo list', () => {
+      expect(myTodoList.indexControl()).toBe(3);
+    });
+
+    test('Delete the Todo at a given index', () => {
+      myTodoList.delete(2);
+      expect(myTodoList.list).toEqual([new Todo(1, false, 'Task 1')]);
+    });
+
+    test('Swap the position of two Todos in the list', () => {
+      myTodoList.new('Task 2');
+      myTodoList.swap(1, 2);
+      expect(myTodoList.list).toEqual([new Todo(2, false, 'Task 2'), 
+      new Todo(1, false, 'Task 1')]);
+    });
 });
